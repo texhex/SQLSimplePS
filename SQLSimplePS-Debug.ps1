d5cd3f29fab2a26659adfe984e158b3490e9cb83
Michael Hex <texhex@users.noreply.github.com>
2018-03-04 20:17:43 +0100

    Placeholder for now

---
+++ SQLSimplePS-Debug.ps1
@@ -0,0 +1,10 @@
+#This script requires PowerShell 5.1 because we are using classes
+#requires -version 5
+
+#Guard against common code errors
+Set-StrictMode -version 2.0
+
+#Terminate script on errors 
+$ErrorActionPreference = 'Stop'
+
+
